f12d3353ef6946c4d1e32233d1ce0b9c622f0c39
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-21 13:50:50 +0100

    Local testing with Selenium Standalone server

---
+++ tests/sampleTest.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Sample test' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
+  }
+};
